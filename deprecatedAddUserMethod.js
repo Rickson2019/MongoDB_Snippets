5079bbf570b6d020e7d75c3f5196f6f3ac0443dd
Rickson2019 <56167026+Rickson2019@users.noreply.github.com>
2021-02-24 20:37:50 -0800

    Update deprecatedAddUserMethod.js

--- deprecatedAddUserMethod.js
+++ deprecatedAddUserMethod.js
@@ -1,5 +1,5 @@
-# NODE.JS add user snippets(deprecated after Mongod v2.4)
-# Add user to database
+// # NODE.JS add user snippets(deprecated after Mongod v2.4)
+// # Add user to database
 
 var MongoClient = require('mongodb').MongoClient
 
@@ -10,9 +10,9 @@ var MongoClient = require('mongodb').MongoClient
 var mongoPort = '27017'
 var mongoHost = 'localhost'
 
-var dbName = 'voting_station'
-var userName = 'poll_admin'
-var userPassword = 'pollination'
+var dbName = 'voting_station'         // The database you wanna add the user to.
+var userName = 'poll_admin'           // The username you want to create. 
+var userPassword = 'pollination'      // The user password you want to use for the user.
 
 var collectionName = 'votes'
 
@@ -20,7 +20,8 @@ var collectionName = 'votes'
 //  start
 //
 
-// +dbName
+// '+dbName' is not needed, because you are connecting to the database using root(db admin account created earlier)
+// if you want to use some other username , then you must specify where the user was added to.
 MongoClient.connect('mongodb://root:root@' + mongoHost + ':' + mongoPort + '/',
     function (err, db) {
 
@@ -64,9 +65,9 @@ MongoClient.connect('mongodb://root:root@' + mongoHost + ':' + mongoPort + '/',
                             console.log("Collection created!");
                             db.close();
                         });
-                    });
+                 });
 
-  });
+            });
 
 
             })
